6a292a6de82731c5021f4f60b3005ed480d6ca1d
adshidtadka <nextage900fx@gmail.com>
2023-06-25 12:09:00 +0900

    index page

--- 2_layout/layout_samples/lib/main.dart
+++ 2_layout/layout_samples/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'grid_view_page.dart';
+import 'index_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const GridViewPage(),
+      home: const IndexPage(),
     );
   }
 }
---
+++ 2_layout/layout_samples/lib/sliver_app_bar_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SliverAppBarPage extends StatefulWidget {
+  @override
+  _SliverAppBarPageState createState() => _SliverAppBarPageState();
+}
+
+class _SliverAppBarPageState extends State<SliverAppBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
