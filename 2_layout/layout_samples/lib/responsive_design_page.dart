e4f322db7a886806ebcc57c777afe5cbbd2ed586
adshidtadka <nextage900fx@gmail.com>
2023-06-28 23:58:07 +0900

    make responsive

--- 2_layout/layout_samples/lib/responsive_design_page.dart
+++ 2_layout/layout_samples/lib/responsive_design_page.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 class ResponsiveDesignPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.grey.shade600,
       appBar: AppBar(
-        title: const Text('さまざまなデバイスに対応する'),
+        title: Text(
+          'さまざまなデバイスに対応する',
+          style: TextStyle(
+            fontSize: 15 * (height + width) / (926 + 438),
+          ),
+        ),
       ),
-      body: const _Body(),
+      body: _Body(
+        width: width,
+        height: height,
+      ),
     );
   }
 }
@@ -16,82 +26,85 @@ class ResponsiveDesignPage extends StatelessWidget {
 class _Body extends StatelessWidget {
   const _Body({
     Key? key,
+    required this.width,
+    required this.height,
   }) : super(key: key);
+
+  final double width;
+  final double height;
 
   @override
   Widget build(BuildContext context) {
+    final isPortrait = height > width;
     return Center(
-      child: Column(children: <Widget>[
-        const SizedBox(
-          height: 60,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50),
-          child: Text(
-            "Flutter is Google's UI tooklit for building beautiful, natively compiled",
-            textAlign: TextAlign.center,
-            style: TextStyle(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 650),
+        child: Column(children: <Widget>[
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Flutter is Google's UI tooklit for building beautiful, natively compiled",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15 * (height + width) / (926 + 438),
+                  height: 1.85,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Spacer(),
+          isPortrait
+              ? FlutterLogo(
+                  size: this.height * 0.3,
+                )
+              : SizedBox(),
+          isPortrait ? Spacer() : SizedBox(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ',
+              textAlign: TextAlign.center,
+              style: TextStyle(
                 color: Colors.white,
-                fontSize: 16,
+                fontSize: 15 * (height + width) / (926 + 438),
                 height: 1.85,
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        FlutterLogo(
-          size: 240,
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50),
-          child: Text(
-            'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ,',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              height: 1.85,
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        SizedBox(
-          height: 42,
-          width: 300,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              textStyle: TextStyle(
-                color: Colors.white,
-              ),
-              primary: Theme.of(context).colorScheme.secondary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
               ),
             ),
-            onPressed: () {},
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                'get started',
-                textAlign: TextAlign.center,
-                style: TextStyle(
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
                     color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
+                  ),
+                  primary: Theme.of(context).colorScheme.secondary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    'get started',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15 * (height + width) / (926 + 438),
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-      ]),
+          Spacer(),
+        ]),
+      ),
     );
   }
 }
