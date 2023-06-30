3b028fe5b97297da3a4245540d2f03f3e043e79b
adshidtadka <nextage900fx@gmail.com>
2023-07-01 00:00:02 +0900

    scale rorate

--- 3_animation/animation_samples/lib/transition_page.dart
+++ 3_animation/animation_samples/lib/transition_page.dart
@@ -40,9 +40,98 @@ class TransitionPage extends StatelessWidget {
                   }),
                 );
               },
-              child: Text('遷移'),
+              child: Text('スライド'),
             ),
-          )
+          ),
+          SizedBox(
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                      return _PageA();
+                    }, transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                      return ScaleTransition(
+                        scale: Tween(begin: 0.0, end: 1.0).animate(
+                            CurvedAnimation(
+                                parent: animation, curve: Curves.easeOutCubic)),
+                        child: child,
+                      );
+                    }),
+                  );
+                },
+                child: Text('スケール'),
+              )),
+          SizedBox(
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                      return _PageA();
+                    }, transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                      return RotationTransition(
+                        turns: Tween(begin: 0.0, end: 2.0).animate(
+                            CurvedAnimation(
+                                parent: animation, curve: Curves.easeOutCubic)),
+                        child: child,
+                      );
+                    }),
+                  );
+                },
+                child: Text('回転'),
+              )),
+          SizedBox(
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                      return _PageA();
+                    }, transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                      return SlideTransition(
+                        position: Tween(
+                                begin: const Offset(-1, -1),
+                                end: const Offset(0, 0))
+                            .animate(CurvedAnimation(
+                                parent: animation, curve: Curves.easeOutCubic)),
+                        child: ScaleTransition(
+                          scale: Tween(begin: 0.0, end: 1.0).animate(
+                              CurvedAnimation(
+                                  parent: animation,
+                                  curve: Curves.easeOutCubic)),
+                          child: RotationTransition(
+                            turns: Tween(begin: 0.0, end: 2.0).animate(
+                                CurvedAnimation(
+                                    parent: animation,
+                                    curve: Curves.easeOutCubic)),
+                            child: child,
+                          ),
+                        ),
+                      );
+                    }),
+                  );
+                },
+                child: Text('組み合わせ'),
+              )),
         ],
       ),
     ));
