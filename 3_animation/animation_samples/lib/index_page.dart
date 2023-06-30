4795beb7c516e1ee224a0a40a6a849b990a4a225
adshidtadka <nextage900fx@gmail.com>
2023-06-30 23:52:43 +0900

    slide animation

--- 3_animation/animation_samples/lib/index_page.dart
+++ 3_animation/animation_samples/lib/index_page.dart
@@ -5,7 +5,7 @@ import 'hero_page.dart';
 // import 'open_container_page.dart';
 // import 'tween_staggered_page.dart';
 // import 'shared_axis_transition_page.dart';
-// import 'transition_page.dart';
+import 'transition_page.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({
@@ -16,19 +16,19 @@ class IndexPage extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-          //   child: ElevatedButton(
-          //     onPressed: () {
-          //       Navigator.of(context).push(
-          //         MaterialPageRoute(builder: (context) {
-          //           return const TransitionPage();
-          //         }),
-          //       );
-          //     },
-          //     child: const Text('Transition'),
-          //   ),
-          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return TransitionPage();
+                  }),
+                );
+              },
+              child: const Text('Transition'),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
             child: ElevatedButton(
