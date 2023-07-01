8e5920ebd83e62b0a94a88a1bf086ac59a933613
adshidtadka <nextage900fx@gmail.com>
2023-07-01 09:14:32 +0900

    animation_controller_page

--- 3_animation/animation_samples/lib/index_page.dart
+++ 3_animation/animation_samples/lib/index_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// import 'animation_controller_page.dart';
+import 'animation_controller_page.dart';
 import 'hero_page.dart';
 import 'open_container_page.dart';
 // import 'tween_staggered_page.dart';
@@ -68,19 +68,19 @@ class IndexPage extends StatelessWidget {
               child: const Text('SharedAxisTransition'),
             ),
           ),
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-          //   child: ElevatedButton(
-          //     onPressed: () {
-          //       Navigator.of(context).push(
-          //         MaterialPageRoute(builder: (context) {
-          //           return const AnimationControllerPage();
-          //         }),
-          //       );
-          //     },
-          //     child: const Text('AnimationController'),
-          //   ),
-          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return const AnimationControllerPage();
+                  }),
+                );
+              },
+              child: const Text('AnimationController'),
+            ),
+          ),
           // Padding(
           //   padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
           //   child: ElevatedButton(
--- 3_animation/animation_samples/lib/open_container_page.dart
+++ 3_animation/animation_samples/lib/open_container_page.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/material.dart';
 
 class OpenContainerImages {
   static const List<String> titles = [
